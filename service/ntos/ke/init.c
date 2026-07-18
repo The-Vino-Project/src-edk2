c754b78a6426d314c68991e115ee7733abd1227d
Chloe Pinrip <chloe@pinrip.null>
2026-07-18 08:18:51 +0000

    ntos: Add kernel C entry
    Signed-off-by: Chloe Pinrip <chloe@pinrip.null>

---
+++ service/ntos/ke/init.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026, pinrip, rixel, et al.
+ *
+ * Abstract:
+ *      Kernel bring up core
+ * Author:
+ *      Chloe Pinrip
+ */
+
+#include <ntdef.h>
+
+VOID
+NtP1Init(VOID)
+{
+}
