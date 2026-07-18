53bf83407b6fd5f5e756c5933da6cb3a8b752964
Chloe Pinrip <chloe@pinrip.null>
2026-07-18 05:16:56 +0000

    boot: loader: Read section headers and dump them
    Signed-off-by: Chloe Pinrip <chloe@pinrip.null>

--- service/boot/app/bootmgr/core/loader.c
+++ service/boot/app/bootmgr/core/loader.c
@@ -16,7 +16,11 @@ EFI_STATUS
 BootLoadKernel(VOID)
 {
     PPE32_HEADER PeHeader;
+    PPE32P_SECTION_HEADER SectionHeaderBase;
+    PPE32P_SECTION_HEADER SectionHeader;
+    PPE32P_OPT_HEADER OptHeader;
     UINT32 *PeHeaderOff;
+    USHORT SectionIdx, NumberOfSections;
 
     PeHeaderOff = PTR_OFFSET(gKernelImage, PE_E_LFANEW_OFF);
     PeHeader = PTR_OFFSET(gKernelImage, *PeHeaderOff);
@@ -25,5 +29,23 @@ BootLoadKernel(VOID)
         return EFI_LOAD_ERROR;
     }
 
+    /* Get the optional header and base of section headers */
+    OptHeader = PTR_OFFSET(PeHeader, sizeof(PE32_HEADER));
+    SectionHeaderBase = PTR_OFFSET(
+        OptHeader,
+        PeHeader->SizeOfOptionalHeader
+    );
+
+    /* Iterate every section header */
+#define SECTION_HEADER(IDX) \
+    PTR_OFFSET(SectionHeaderBase, (IDX) * sizeof(PE32P_SECTION_HEADER))
+    NumberOfSections = PeHeader->NumberOfSections;
+    for (SectionIdx = 0; SectionIdx < NumberOfSections; ++SectionIdx) {
+        SectionHeader = SECTION_HEADER(SectionIdx);
+        Print(L"Section name : %a\r\n", SectionHeader->Name);
+        Print(L"Section RVA : %p\r\n", SectionHeader->VirtualAddress);
+        Print(L"Section VSIZE : %d\r\n", SectionHeader->VirtualSize);
+    }
+#undef SECTION_HEADER
     return EFI_SUCCESS;
 }
